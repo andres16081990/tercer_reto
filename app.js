26fdb3a616905060f5f56f548ecd42da5cd0a8b5
Joaquin <andres16081990@gmail.com>
2021-06-24 10:50:59 -0500

    perro con perro

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const app = express();
 
 app.get('/',(req,res)=>{
     const {nombre} = req.query;
-    res.status(200).send(`<h1>Hola ${nombre}!</h1>`)
+    if(nombre){
+        res.status(200).send(`<h1>Hola ${nombre}!</h1>`)
+    }
+    if(!nombre){
+        res.status(200).send(`<h1>Hola desconocido!</h1>`)
+    }    
 })
 
 app.listen(3000,()=>console.log('listening from at port 3000'));
